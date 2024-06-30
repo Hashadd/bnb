f3eaacde76eb5f83b05c097707a72f9cab560571
Ahmed Hashad <104703218+Hashadd@users.noreply.github.com>
2024-06-30 11:40:50 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $('.categoury-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    centerMode: true,
+
+  });
+
+})
+
